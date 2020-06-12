0f082c8aeab5a2e5e7cac8269b1a6cb37ed0c993
alapvi <64133017+alapvi@users.noreply.github.com>
2020-06-12 18:10:16 +0200

    version1

---
+++ Sistema/Logger.ps1
@@ -0,0 +1,20 @@
+﻿#Creamos un fichero de log
+$logfile = ".\Log\powershell.log"
+if (-not (Test-Path  $logfile) ) {
+    New-Item -Path $logfile -Force
+}
+
+#Definimos el tiempo
+$Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+
+#Ejecutamos la acción 
+$maxIdCPUProcess = (Get-Process | Sort-Object CPU -Descending |Select-Object -First 1).Id
+$ProcessName = (Get-Process -Id $maxIdCPUProcess).Name
+$ProcessCPU = (Get-Process -Id $maxIdCPUProcess).CPU
+
+#Añadimos al fichero log
+$Line = "$Stamp $maxIdCPUProcess $ProcessName $ProcessCPU"
+Add-Content -Path $logfile -Value $line
+
+#Consultamos fichero
+Get-Content -Path $logfile
