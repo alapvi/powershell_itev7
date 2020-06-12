ccbe330ef8ecfd20176d99d9b8e326c7c32f1dd0
alapvi <64133017+alapvi@users.noreply.github.com>
2020-06-12 09:05:58 +0200

    Create CheckHomes.ps1

---
+++ Usuarios/CheckHomes.ps1
@@ -0,0 +1,9 @@
+﻿#Comprobación de carpetas personales de usuarios
+$usuarios = Get-LocalUser | Where-Object Name
+foreach ($i in $usuarios) {
+    If ( Test-Path "C:\Users\$i" ) {
+        Write-Host "Usuario $i Tiene home"
+    } else {
+        Write-Host "Usuario $i NO tiene home"
+    }
+} 
