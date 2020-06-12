a72dd4419342705aad9b88ebb85bf153d673b82a
alapvi <64133017+alapvi@users.noreply.github.com>
2020-06-12 09:01:21 +0200

    Delete Prueba.ps1

--- Prueba.ps1
+++
@@ -1,9 +0,0 @@
-﻿$jugadores = New-Object System.Collections.Hashtable
-do {
-    [Int]$num = Read-Host "Introduce Numero dorsal"
-    if ($num -eq 0) { break }
-    $nombre = Read-Host "Introduce Nombre"
-    $jugadores.Add($num,$nombre)
-} while ($num -ne 0)
-
-$jugadores.Item(1)
