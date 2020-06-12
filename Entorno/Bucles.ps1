e8ce50d77776642fc7c905d5d371215f7fa7307c
alapvi <64133017+alapvi@users.noreply.github.com>
2020-06-12 11:08:24 +0200

    version2

--- Entorno/Bucles.ps1
+++ Entorno/Bucles.ps1
@@ -1,40 +1,49 @@
 ﻿
 #Números del 1 al 100 con bucle while
+Write-Host "Bucle While"
 $x=1
 while ($x -le 100)
 {
-    Write-Host $x
+    Write-Host $x " " -NoNewline
     $x++ #incrementa en uno la variable $x
 }
+Write-Host ""
 
 
 #Números del 1 al 100 con bucle do-until
+Write-Host "Bucle Do-Until"
 $x=1
 do
 {
-    Write-Host $x 
+    Write-Host $x " " -NoNewline
     $x++ #incrementa en uno la variable $x
 }
 until ($x -gt 100)
+Write-Host ""
 
+Write-Host "Bucle Do-While"
 #Números del 1 al 100 con bucle do-while
 $x=0
 do
 {
     $x++
-    Write-Host $x
+    Write-Host $x " " -NoNewline
 } while ($x -lt 100)
+Write-Host ""
 
+Write-Host "Bucle For"
 #Números del 1 al 100 con bucle for
 for ($x = 1; $x -le 100; $x++)
 { 
-    Write-Host $x
+    Write-Host $x " " -NoNewline
 }
+Write-Host ""
 
-
+Write-Host "Bucle Foreach"
 #Números del 1 al 100 con bucle foreach
 $numeros= 1..100 #variable de tipo colección
 foreach ($x in $numeros)
 { 
-    Write-Host $x
+    Write-Host $x " " -NoNewline
 }
+Write-Host ""
---
+++ Usuarios/NumUsuarios.ps1
@@ -0,0 +1,9 @@
+﻿#Cuenta el número de usuarios que hay en nuestro sistema mediante estructuras repetitivas
+$total = 0
+$usuarios = Get-LocalUser | Select-Object Name
+foreach ($usu in $usuarios) {
+    Write-Host "Usuario encontrado $($usu.Name)"
+    $total++
+}
+Write-Host "El total de usuarios es $total"
+
