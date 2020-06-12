45505899753294220728f0fa1156f15e5bdab8ab
alapvi <64133017+alapvi@users.noreply.github.com>
2020-06-12 17:27:50 +0200

    Create ConexionesTCP.ps1

---
+++ Sistema/ConexionesTCP.ps1
@@ -0,0 +1,6 @@
+﻿#Obtener aquellos procesos que tienen conexiones TCP abiertas
+$conexiones = Get-NetTCPConnection -State Established | Select-Object OwningProcess -Unique 
+foreach ($proc in $conexiones) {
+    Get-Process -Id $proc.OwningProcess
+}
+
