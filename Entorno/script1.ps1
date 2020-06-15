be731d501c51e551326a1a65b614e72d44c21072
alapvi <64133017+alapvi@users.noreply.github.com>
2020-06-15 10:43:52 +0200

    Update script1.ps1

--- Entorno/script1.ps1
+++ Entorno/script1.ps1
@@ -1,4 +1,12 @@
-﻿New-Variable -Name PI -Value 3.1415 -Option Constant
-$radio = 45.7
+﻿#Creamos una variable de Tipo String
 $nombre = "Alberto"
+
+#Creamos una variable de tipo Int
 $edad = 45
+
+#Creamos una variable de tipo Double
+$radio = 45.7
+
+#Creamos una variable de tipo Boolean
+$mayorEdad = $True
+
